a9109b54b2b448a351d68598a10aff65dde652e1
Bob <37924224+boby177@users.noreply.github.com>
2020-08-07 22:15:34 +0700

    Tugas Besar Analisis Design Pattern CCDP

--- Structural/src/Prototype/Message.java
+++
@@ -1,7 +0,0 @@
-import java.util.Date;
-
-public class Message {
-    public static void showMessage(Pelayan user, String message){
-        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
-    }
-}
